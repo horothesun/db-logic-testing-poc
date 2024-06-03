a6b097ceda4980354713585d98b8648c9c756f3e
Scala Steward <horothesun@gmail.com>
2024-06-03 13:47:51 +0200

    Update doobie-core, doobie-postgres to 1.0.0-RC5

--- Main.scala
+++ Main.scala
@@ -2,8 +2,8 @@
 //> using jvm "temurin:21"
 
 //> using dep "org.typelevel::cats-effect:3.5.4"
-//> using dep "org.tpolecat::doobie-core:1.0.0-RC4"
-//> using dep "org.tpolecat::doobie-postgres:1.0.0-RC4"
+//> using dep "org.tpolecat::doobie-core:1.0.0-RC5"
+//> using dep "org.tpolecat::doobie-postgres:1.0.0-RC5"
 
 //> using test.dep "org.scalameta::munit:1.0.0"
 //> using test.dep "org.typelevel::munit-cats-effect:2.0.0"
