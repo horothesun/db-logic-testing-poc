b54e7a9befc480a186279396ae572fe0ad5c2391
horothesun <horothesun@gmail.com>
2024-10-08 23:47:14 +0100

    scalafmt: imports sorting

--- integration/Integration.test.scala
+++ integration/Integration.test.scala
@@ -3,6 +3,5 @@ import munit.CatsEffectSuite
 
 class IntegrationSuite extends CatsEffectSuite:
 
-  test("pure_db_logic(123) returns 42") {
+  test("pure_db_logic(123) returns 42"):
     DbClient.postgres.pureDbLogic(123).assertEquals(Some(42))
-  }
--- unit/Unit.test.scala
+++ unit/Unit.test.scala
@@ -2,6 +2,5 @@ import munit.FunSuite
 
 class UnitSuite extends FunSuite:
 
-  test("1+1 is 2") {
+  test("1+1 is 2"):
     assertEquals(1 + 1, 2)
-  }
